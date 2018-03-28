08314e1582702da724e2751c3230863e7b579506
ki-yamada <ki-yamada@ki-yamada-MB662.local>
2018-03-28 13:28:03 +0900

    stateのベースクラス作成

---
+++ CampTask/Assets/CampTask/Scripts/StateBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateBase : IState
+{
+    protected Player _Player;
+
+    public StateBase(Player player)
+    {
+        _Player = player;
+    }
+
+    public abstract bool CanShot();
+
+    public virtual InputStatus GetInputStatus()
+    {
+        InputStatus inputStatus = new InputStatus();
+        if (Input.GetMouseButtonDown(0))
+        {
+            inputStatus.IsShot = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            inputStatus.IsRun = true;
+
+        }
+        return inputStatus;
+    }
+}
